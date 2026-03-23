aad9fdb7e191322966fa9545ab48c55b36d9fc35
xentriom <c04jason@gmail.com>
2026-03-23 16:54:01 -0400

    update build script to populate nuqs options

--- lib/search-params.ts
+++ lib/search-params.ts
@@ -6,16 +6,20 @@ import {
   type inferParserType,
 } from "nuqs/server";
 
-export const yearOptions = [2020, 2021, 2022, 2023, 2024];
-export const monthOptions = ["1"];
-export const agencyOptions = ["1"];
-export const offenseCodeOptions = ["1"];
+import {
+  yearOptions,
+  monthOptions,
+  agencyOptions,
+  offenseCodeOptions,
+} from "./nuqs-options.generated";
+
+export * from "./nuqs-options.generated";
 
 export const searchParams = {
   year: parseAsNumberLiteral(yearOptions).withDefault(2024),
-  month: parseAsStringLiteral(monthOptions).withDefault("1"),
-  agencyId: parseAsStringLiteral(agencyOptions).withDefault("1"),
-  offenseCode: parseAsStringLiteral(offenseCodeOptions).withDefault("1"),
+  month: parseAsStringLiteral(monthOptions).withDefault("all"),
+  agencyId: parseAsStringLiteral(agencyOptions).withDefault("all"),
+  offenseCode: parseAsStringLiteral(offenseCodeOptions).withDefault("all"),
 };
 
 export type SearchParams = inferParserType<typeof searchParams>;
