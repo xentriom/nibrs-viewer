ee3e3bc6ddd80edd8968478b9d80e21a129b2531
xentriom <c04jason@gmail.com>
2026-03-23 14:18:38 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <main className="grid h-dvh grid-rows-[3fr_1fr] gap-4 p-4">
       <div className="grid grid-cols-[3fr_1fr] rounded-md">
-        <div className="rounded-md">e</div>
-        <div className="rounded-md bg-sidebar">a</div>
+        <div className="rounded-md">map</div>
+        <div className="rounded-md bg-sidebar">controls</div>
       </div>
-      <div className="rounded-md bg-sidebar">g</div>
+      <div className="rounded-md bg-sidebar">graph</div>
     </main>
   );
 }
